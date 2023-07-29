c9a1484f7af5680d3caafcb926db87bf47158cb1
Sam Chung <chungsam@cityu.edu>
2023-07-29 15:47:28 +0000

    HOS05A

---
+++ Module 05/client-src-examples/section4/App.js
@@ -0,0 +1,27 @@
+// Section 4
+// https://react.dev/learn/managing-state#passing-data-deeply-with-context
+import Heading from './Heading.js';
+import Section from './Section.js';
+
+export default function Page() {
+  return (
+    <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
