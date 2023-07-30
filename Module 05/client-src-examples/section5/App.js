77975b403ce21cfe9d42ad482633877ef7120d99
Sam Chung <chungsam@cityu.edu>
2023-07-30 12:25:15 +0000

    HOS05

---
+++ Module 05/client-src-examples/section5/App.js
@@ -0,0 +1,18 @@
+// Section 5
+// https://react.dev/learn/escape-hatches#referencing-values-with-refs
+import { useRef } from 'react';
+
+export default function Counter() {
+  let ref = useRef(0);
+
+  function handleClick() {
+    ref.current = ref.current + 1;
+    alert('You clicked ' + ref.current + ' times!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me!
+    </button>
+  );
+}
---
+++ Module 05/client-src-examples/section6/App.js
@@ -0,0 +1,21 @@
+//Section 6
+// https://react.dev/learn/escape-hatches#manipulating-the-dom-with-refs
+
+import { useRef } from 'react';
+
+export default function Form() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleClick}>
+        Focus the input
+      </button>
+    </>
+  );
+}
