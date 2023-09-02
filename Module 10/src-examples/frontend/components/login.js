c59fd7777111f9b13aec26365024ffcdfc2209f1
Sam Chung <chungsam@cityu.edu>
2023-09-02 22:31:36 +0000

    HOS10A

---
+++ Module 10/src-examples/frontend/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="yourBackendURL/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
